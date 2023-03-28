67e4054cc1f9606fb217bbe968c5b62278445524
Andres.Dev-PersonalNbk <esmicuentapersonal@gmail.com>
2023-03-28 02:29:05 -0300

    initial files project

---
+++ src/main/java/com/betasepp/qr/QrCodeGeneratorReaderApplication.java
@@ -0,0 +1,13 @@
+package com.betasepp.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeGeneratorReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeGeneratorReaderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/betasepp/qr/configuration/properties/OpenApiConfigurationProperties.java
@@ -0,0 +1,30 @@
+
+package com.betasepp.qr.configuration.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "com.betasepp.qr.swagger")
+public class OpenApiConfigurationProperties {
+
+	private Properties properties = new Properties();
+
+	@Data
+	public class Properties {
+		private String title;
+		private String description;
+		private String apiVersion;
+
+		private Contact contact = new Contact();
+
+		@Data
+		public class Contact {
+			private String email;
+			private String name;
+			private String url;
+		}
+	}
+
+}
